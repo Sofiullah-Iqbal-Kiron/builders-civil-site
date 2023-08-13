5abb43206dc3074f9eaf8789c7896f49bfa03a96
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2023-08-13 10:31:16 +0600

    add background image to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,9 @@
 import {motion} from 'framer-motion';
-import {FaRightLong} from "react-icons/fa6";
 
 export function Hero() {
     return (
         <section id="#hero"
-                 className='sm:min-h-screen sm:ps-10 flex flex-col justify-center items-center sm:items-start space-y-10'>
+                 className="bg-[url('assets/img-3.jpg')] bg-cover bg-blend-normal sm:min-h-screen sm:ps-10 flex flex-col justify-center items-center sm:items-start space-y-10">
             <motion.p
                 initial={{opacity: 0}}
                 animate={{opacity: 1}}
@@ -18,27 +17,25 @@ export function Hero() {
                 initial={{y: 20, opacity: 0}}
                 animate={{y: 0, opacity: 1}}
                 transition={{delay: 0.5, duration: 0.5}}
-                className='text-5xl sm:text-7xl text-center sm:text-left font-serif text-red-700 font-semibold sm:font-bold'>
+                className='bg-white/30 backdrop-blur-sm backdrop-opacity-50 p-5 rounded text-5xl sm:text-7xl text-center sm:text-left font-serif text-red-600 font-semibold sm:font-bold'>
                 Global construction <br/> and Engineering
             </motion.p>
             <motion.p
                 initial={{y: 20, opacity: 0}}
                 animate={{y: 0, opacity: 1}}
                 transition={{delay: 1, duration: 0.5}}
-                className='text-center sm:text-left'
+                className='text-center sm:text-left font-semibold'
             >
-                Experience and sustainability - these are the keys to success of Byron<br/>for the realisation of large
+                Experience and sustainability - these are the keys to success of Byron<br/>for the realisation of
+                large
                 international construction projects.
             </motion.p>
             <motion.p
                 initial={{x: -100, opacity: 0}}
                 animate={{x: 0, opacity: 1}}
                 transition={{delay: 1.5, duration: 0.4}}
+                className='pb-2'
             >
-                {/*<a href='#get-started'*/}
-                {/*   className='px-4 py-3 bg-gradient-to-r from-yellow-500 to-yellow-200 shadow-xl text-gray-800 font-bold text-md sm:text-2xl rounded flex justify-center items-center space-x-10 w-80'>*/}
-                {/*    <p>Get Started</p> <FaRightLong/>*/}
-                {/*</a>*/}
                 <a
                     type="button"
                     href='#get-started'
