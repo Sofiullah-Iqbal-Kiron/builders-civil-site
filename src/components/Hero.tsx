b24092f934a606b0a4f67545275ad17bef4fbe3a
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2023-08-13 09:36:23 +0600

    fix get-started button and sites title font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,8 @@ import {FaRightLong} from "react-icons/fa6";
 
 export function Hero() {
     return (
-        <section id="#hero" className='sm:min-h-screen sm:ps-10 flex flex-col justify-center items-center sm:items-start space-y-10'>
+        <section id="#hero"
+                 className='sm:min-h-screen sm:ps-10 flex flex-col justify-center items-center sm:items-start space-y-10'>
             <motion.p
                 initial={{opacity: 0}}
                 animate={{opacity: 1}}
@@ -17,7 +18,7 @@ export function Hero() {
                 initial={{y: 20, opacity: 0}}
                 animate={{y: 0, opacity: 1}}
                 transition={{delay: 0.5, duration: 0.5}}
-                className='text-5xl sm:text-7xl text-center sm:text-left font-tiltprism-400 text-red-700 font-semibold sm:font-bold'>
+                className='text-5xl sm:text-7xl text-center sm:text-left font-serif text-red-700 font-semibold sm:font-bold'>
                 Global construction <br/> and Engineering
             </motion.p>
             <motion.p
@@ -34,8 +35,21 @@ export function Hero() {
                 animate={{x: 0, opacity: 1}}
                 transition={{delay: 1.5, duration: 0.4}}
             >
-                <a href='#get-started' className='px-4 py-3 bg-gradient-to-r from-yellow-500 to-yellow-200 shadow-xl text-gray-800 font-bold text-md sm:text-2xl rounded flex justify-center items-center space-x-10 w-80'>
-                    <p>Get Started</p> <FaRightLong/>
+                {/*<a href='#get-started'*/}
+                {/*   className='px-4 py-3 bg-gradient-to-r from-yellow-500 to-yellow-200 shadow-xl text-gray-800 font-bold text-md sm:text-2xl rounded flex justify-center items-center space-x-10 w-80'>*/}
+                {/*    <p>Get Started</p> <FaRightLong/>*/}
+                {/*</a>*/}
+                <a
+                    type="button"
+                    href='#get-started'
+                    className="py-3 px-4 py-3 px-4 inline-flex justify-center items-center gap-5 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800">
+                    <p className='text-2xl'>Get started</p>
+                    <svg className="w-5 h-auto" width="17" height="16" viewBox="0 0 17 16" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M1 7C0.447715 7 -3.73832e-07 7.44771 -3.49691e-07 8C-3.2555e-07 8.55228 0.447715 9 1 9L13.0858 9L7.79289 14.2929C7.40237 14.6834 7.40237 15.3166 7.79289 15.7071C8.18342 16.0976 8.81658 16.0976 9.20711 15.7071L16.0303 8.88388C16.5185 8.39573 16.5185 7.60427 16.0303 7.11612L9.20711 0.292893C8.81658 -0.0976318 8.18342 -0.0976318 7.79289 0.292893C7.40237 0.683417 7.40237 1.31658 7.79289 1.70711L13.0858 7L1 7Z"
+                              fill="currentColor"/>
+                    </svg>
                 </a>
             </motion.p>
         </section>
--- src/components/ServiceLink.tsx
+++ src/components/ServiceLink.tsx
@@ -6,7 +6,7 @@ interface Props {
 export function ServiceLink({icon, name}: Props) {
     return (
         <a href={`#${name}`}
-           className='flex flex-col justify-evenly items-center h-[10rem] w-[10rem] rounded-xl ring-1 ring-sky-800 bg-gradient-to-br from-sky-400 to-sky-200 text-gray-800 font-bold hover:from-sky-500 hover:to-sky-300 hover:scale-105 transition-all'>
+           className='flex flex-col justify-evenly items-center h-[10rem] w-[10rem] rounded-xl ring-1 ring-sky-800 bg-gradient-to-br from-sky-300 to-sky-200 text-gray-800 font-bold hover:from-sky-400 hover:to-sky-200 transition-all duration-300'>
             <div className='text-4xl'>{icon}</div>
             <p>{name}</p>
         </a>
